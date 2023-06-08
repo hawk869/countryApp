42f393770d7e6d4d541ed4329b99df4871a55326
Daniel Esteban <decepeda@misena.edu.co>
2023-06-08 16:11:58 -0500

    Creamos el modulo router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     SharedModule
   ],
   providers: [],
