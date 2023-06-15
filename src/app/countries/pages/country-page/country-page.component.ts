941bc584511e1d094acbf9a04de94d95dbb0b3ad
Daniel Esteban <decepeda@misena.edu.co>
2023-06-15 18:06:33 -0500

    Argumentos por URL

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-country-page',
@@ -6,6 +8,22 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+
+  constructor( 
+    private activateRoute: ActivatedRoute,
+    private countriesService: CountriesService 
+    ) {}
+
+  ngOnInit(): void {
+    this.activateRoute.params.subscribe( ({ id }) => {
+      // console.log({ params: id })
+      this.countriesService.searchCountryByAlphaCode( id )
+        .subscribe( country => {
+          console.log( country );
+          
+        })
+    });
+  }
 
 }
