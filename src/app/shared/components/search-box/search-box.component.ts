c125c609cf7d2800886e890abd67646dc6ecc547
Daniel Esteban <decepeda@misena.edu.co>
2023-06-17 20:03:03 -0500

    OnDestroy limpieza de subscripciones

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Subject, debounceTime } from 'rxjs';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
@@ -7,9 +7,10 @@ import { Subject, debounceTime } from 'rxjs';
   styles: [
   ]
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements OnInit, OnDestroy {
 
   private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSubscription?: Subscription;
 
   @Input()
   public placeholder: string = '';
@@ -21,13 +22,17 @@ export class SearchBoxComponent implements OnInit {
   onDebounce = new EventEmitter<string>();
 
   ngOnInit(): void {
-    this.debouncer
+    this.debouncerSubscription = this.debouncer
       .pipe(
         debounceTime( 400 )
       )
       .subscribe( value => {
         this.onDebounce.emit( value );
       })
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSubscription?.unsubscribe;
   }
 
   emitValue( value: string ): void {
