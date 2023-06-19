caf579ccb1892e1ee0e869af04c6a40c9c993b1d
Daniel Esteban <decepeda@misena.edu.co>
2023-06-19 16:03:35 -0500

    busqueda por region optimizada

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa'|'Americas'|'Asia'|'Europe'|'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,12 +12,17 @@ import { CountriesService } from '../../services/countries.service';
 })
 export class ByRegionPageComponent {
 
+  public regions: Region[] = ['Africa','Americas','Asia','Europe','Oceania'];
+  public selectedRegion?: Region;
+
   @Input()
   public countries: Country[] = [];
 
   constructor( private countriesService: CountriesService ) {}
 
-  searchByRegion( term: string ): void {
+  searchByRegion( term: Region ): void {
+
+    this.selectedRegion = term;
     this.countriesService.searchRegion( term ).subscribe( countries => {
       this.countries = countries;
     })
